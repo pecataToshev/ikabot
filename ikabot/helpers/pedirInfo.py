4531d6d60b29dbf79de5280894866f6d4b58b4d8
Petar Toshev <petar.toshev@designtechnologies.com>
2024-01-01 14:22:08 +0200

    Fix circular dependency and imports

--- ikabot/helpers/pedirInfo.py
+++ ikabot/helpers/pedirInfo.py
@@ -84,6 +84,18 @@ def read(min=None, max=None, digit=False, msg=prompt, values=None, empty=False, 
     return read_input
 
 
+def askUserYesNo(question):
+    """
+    Asks user the yes/no question in the message and returns his response.
+    :param question: question
+    :return: bool
+    """
+    return read(
+        msg=' - {}? (y|N) '.format(question),
+        values=['y', 'Y', 'n', 'N'],
+    ).lower() == 'y'
+
+
 def chooseCity(session, foreign=False):
     """Prompts the user to chose a city
     Parameters
