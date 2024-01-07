5d4c97425e14b929248589dd43f8d0a26148fff8
Petar Toshev <petar.toshev@designtechnologies.com>
2024-01-07 04:10:51 +0200

    Stop self-killing killTasks process

--- ikabot/function/killTasks.py
+++ ikabot/function/killTasks.py
@@ -1,12 +1,11 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-import datetime
 import os
 import sys
 
 from ikabot import config
 from ikabot.config import isWindows
-from ikabot.helpers.gui import banner, enter, formatTimestamp, printTable
+from ikabot.helpers.gui import banner, enter
 from ikabot.helpers.pedirInfo import read
 from ikabot.helpers.process import IkabotProcessListManager, run
 
@@ -28,9 +27,9 @@ def killTasks(session, event, stdin_fd, predetermined_input):
         while True:
             banner()
 
-            process_list = process_list_manager.get_process_list()
-            process_list = [process for process in process_list if process['action'] != 'killTasks']
-            if len(process_list) == 0:
+            process_list = [process for process in process_list_manager.get_process_list()]
+            other_tasks = [process for process in process_list if process['action'] != 'killTasks']
+            if len(other_tasks) == 0:
                 print('There are no tasks running')
                 enter()
                 event.set()
@@ -44,10 +43,17 @@ def killTasks(session, event, stdin_fd, predetermined_input):
                 event.set()
                 return
 
+            process_to_kill = process_list[choice - 1]
+
+            if process_to_kill['action'] == 'killTasks':
+                print('You cannot kill me.... From here...')
+                enter()
+                continue
+
             if isWindows:
-                run("taskkill /F /PID {}".format(process_list[choice-1]['pid']))
+                run("taskkill /F /PID {}".format(process_to_kill['pid']))
             else:
-                run("kill -9 {}".format(process_list[choice-1]['pid']))
+                run("kill -9 {}".format(process_to_kill['pid']))
     except KeyboardInterrupt:
         event.set()
         return
