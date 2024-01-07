cd1822716f027698c4824c29129219230b6f6336
Petar Toshev <petar.toshev@designtechnologies.com>
2024-01-07 11:35:38 +0200

    Fix missed _(str) in getStatus

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="ikabot",
-    version='2024.0107.dev092234',
+    version='2024.0107.dev093538',
     author="Petar Toshev",
     author_email="pecata.toshev+ikabot@gmail.com",
     license='MIT',
