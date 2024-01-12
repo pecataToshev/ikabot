f1e0b43cf3d602d2f67dd98ad53fe1e562c4d36d
Petar Toshev <petar.toshev@designtechnologies.com>
2024-01-12 02:34:38 +0200

    Fix not starting

--- ikabot/__main__.py
+++ ikabot/__main__.py
@@ -1,8 +1,18 @@
+import multiprocessing
+import sys
+
 from ikabot.command_line import start_command_line
 from ikabot.migrations.migrate import apply_migrations
 
-def main():
-    print('Here in the __main__.py')
 
+def main():
     apply_migrations()
-    start_command_line('__main__.py')
+    start_command_line()
+
+
+if __name__ == '__main__':
+    # On Windows calling this function is necessary.
+    if sys.platform.startswith('win'):
+        multiprocessing.freeze_support()
+
+    main()
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="ikabot",
-    version='2024.0111.dev233832',
+    version='2024.0112.dev003438',
     author="Petar Toshev",
     author_email="pecata.toshev+ikabot@gmail.com",
     license='MIT',
