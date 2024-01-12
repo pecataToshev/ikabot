c27d77ae8d2b4c6b2ce939e46b603d0c8611e64e
Petar Toshev <petar.toshev@designtechnologies.com>
2024-01-12 02:37:40 +0200

    Fix input and tests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="ikabot",
-    version='2024.0112.dev003526',
+    version='2024.0112.dev003740',
     author="Petar Toshev",
     author_email="pecata.toshev+ikabot@gmail.com",
     license='MIT',
