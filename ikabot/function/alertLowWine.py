dcee37f92c1413e69f88b4f4d3c9c1fa5a221c2f
Petar Toshev <petar.toshev@designtechnologies.com>
2024-01-15 02:06:19 +0200

    Migrate wine monitoring

--- ikabot/function/alertLowWine.py
+++ ikabot/function/alertLowWine.py
@@ -1,123 +1,32 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-import sys
-import traceback
-from decimal import Decimal, getcontext
-
-from ikabot import config
-from ikabot.config import city_url, SECONDS_IN_HOUR
-from ikabot.helpers.botComm import checkTelegramData, sendToBot
-from ikabot.helpers.getJson import getCity
-from ikabot.helpers.gui import banner, daysHoursMinutes, enter
-from ikabot.helpers.pedirInfo import getIdsOfCities, read
-from ikabot.helpers.ikabotProcessListManager import set_child_mode
-from ikabot.helpers.resources import getProductionPerSecond
-from ikabot.helpers.signals import setInfoSignal
-
-getcontext().prec = 30
+from ikabot.bot.wineMonitoringBot import WineMonitoringBot
+from ikabot.helpers.gui import banner, enter
+from ikabot.helpers.pedirInfo import read
 
 
-def alertLowWine(session, event, stdin_fd, predetermined_input):
+def alertLowWine(ikariam_service, db, telegram):
     """
     Parameters
     ----------
-    session : ikabot.web.ikariamService.IkariamService
-    event : multiprocessing.Event
-    stdin_fd: int
-    predetermined_input : multiprocessing.managers.SyncManager.list
+    ikariam_service : ikabot.web.ikariamService.IkariamService
+    db: ikabot.helpers.database.Database
+    telegram: ikabot.helpers.telegram.Telegram
     """
-    sys.stdin = os.fdopen(stdin_fd)
-    config.predetermined_input = predetermined_input
-    try:
-        if checkTelegramData(session) is False:
-            event.set()
-            return
-        banner()
-        hours = read(msg='How many hours should be left until the wine runs out in a city so that it\'s alerted?', min=1, digit=True)
-        print('It will be alerted when the wine runs out in less than {:d} hours in any city'.format(hours))
-        enter()
+    banner()
 
-    except KeyboardInterrupt:
-        event.set()
+    if not telegram.has_valid_data():
+        print('Telegram data is required to setup the monitoring. Sorry!')
+        enter()
         return
 
-    session.setProcessObjective(
+    hours = read(msg='How many hours should be left until the wine runs out in a city so that it\'s alerted?',
+                 min=1, digit=True)
+    print("You'll will be alerted when the wine runs out in less than {:d} hours in any city".format(hours))
+    enter()
+
+    WineMonitoringBot(ikariam_service, {'minimumWineHours': hours}).start(
         action='Monitor Wine',
-        objective='Wine Hours {}'.format(hours)
+        objective='Wine Hours {}'.format(hours),
     )
-
-    set_child_mode(session)
-    event.set()
-
-    info = '\nI alert if the wine runs out in less than {:d} hours\n'.format(hours)
-    setInfoSignal(session, info)
-    try:
-        do_it(session, hours)
-    except Exception as e:
-        msg = 'Error in:\n{}\nCause:\n{}'.format(info, traceback.format_exc())
-        sendToBot(session, msg)
-    finally:
-        session.logout()
-
-
-def do_it(session, hours):
-    """
-    Parameters
-    ----------
-    session : ikabot.web.ikariamService.IkariamService
-    hours : int
-    """
-
-    was_alerted = {}
-    while True:
-        session.setProcessInfo('Checking for low wine')
-
-        # getIdsOfCities is called on a loop because the amount of cities may change
-        ids, cities = getIdsOfCities(session)
-
-        for cityId in cities:
-            if cityId not in was_alerted:
-                was_alerted[cityId] = False
-
-        for cityId in cities:
-            html = session.get(city_url + cityId)
-            city = getCity(html)
-
-            # if the city doesn't even have a tavern built, ignore it
-            if 'tavern' not in [building['building'] for building in city['position']]:
-                continue
-
-            consumption_per_hour = city['wineConsumptionPerHour']
-
-            # is a wine city
-            if cities[cityId]['tradegood'] == '1':
-                wine_production = getProductionPerSecond(session, cityId)[1] * SECONDS_IN_HOUR
-                if consumption_per_hour > wine_production:
-                    consumption_per_hour -= wine_production
-                else:
-                    was_alerted[cityId] = False
-                    continue
-
-            consumption_per_seg = Decimal(consumption_per_hour) / Decimal(SECONDS_IN_HOUR)
-            wine_available = city['availableResources'][1]
-
-            if consumption_per_seg == 0:
-                if was_alerted[cityId] is False:
-                    msg = 'The city {} is not consuming wine!'.format(city['name'])
-                    sendToBot(session, msg)
-                    was_alerted[cityId] = True
-                continue
-
-            seconds_left = Decimal(wine_available) / Decimal(consumption_per_seg)
-            if seconds_left < hours*60*60:
-                if was_alerted[cityId] is False:
-                    time_left = daysHoursMinutes(seconds_left)
-                    msg = 'In {}, the wine will run out in {}'.format(time_left, city['name'])
-                    sendToBot(session, msg)
-                    was_alerted[cityId] = True
-            else:
-                was_alerted[cityId] = False
-
-        session.wait(20*60, 'I wait for the next check')
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="ikabot",
-    version='2024.0115.dev000601',
+    version='2024.0115.dev000619',
     author="Petar Toshev",
     author_email="pecata.toshev+ikabot@gmail.com",
     license='MIT',
