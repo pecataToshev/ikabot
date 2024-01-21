42ee085d0af02858f8cd3cb8d7e4f09ecc97fd64
Petar Toshev <petar.toshev@designtechnologies.com>
2024-01-21 14:23:01 +0200

    Added tests to TransportGoodsBot.optimize_jobs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="ikabot",
-    version='2024.0121.dev032329',
+    version='2024.0121.dev122741',
     author="Petar Toshev",
     author_email="pecata.toshev+ikabot@gmail.com",
     license='MIT',
