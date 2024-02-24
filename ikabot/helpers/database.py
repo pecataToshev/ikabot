2b38733453b3fab9eadc7c66a2b73cf9afe35830
Petar Toshev <petar.toshev@designtechnologies.com>
2024-02-24 18:38:27 +0200

    Opening cursor fo each query

--- ikabot/helpers/database.py
+++ ikabot/helpers/database.py
@@ -1,6 +1,8 @@
 import json
 import logging
 import sqlite3
+from contextlib import closing
+from typing import List
 
 from ikabot import config
 
@@ -12,12 +14,10 @@ class Database:
     def __init__(self, bot_name):
         logging.debug('Creating db connection; botName=%s', bot_name)
         self.__bot_name = bot_name
-        self.__conn = sqlite3.connect(config.DB_FILE)
-        self.__cursor = self.__conn.cursor()
+        self.__conn = sqlite3.connect(config.DB_FILE, )
 
     def close_db_conn(self):
         logging.debug('Closing db connection')
-        self.__cursor.close()
         self.__conn.close()
 
     def __add_bot_name_to_args(self, args):
@@ -30,55 +30,47 @@ class Database:
         args[self.__bot_name_str] = self.__bot_name
         return args
 
-    def __select(self, table, where=None, args=None):
+    def __select(self, table: str, where:List[str]=None, args:dict=None) -> List[dict]:
         """
         Select data from table
-        :param table: str
-        :param where: list[str]
-        :param args: dict
-        :return:
         """
         where = " AND ".join([self.__bot_name_where] + (where or []))
         args = self.__add_bot_name_to_args(args)
-        self.__cursor.execute(f'SELECT * FROM {table} WHERE {where}', args)
 
-        # Get column names from the cursor description
-        columns = [column[0] for column in self.__cursor.description]
+        with closing(self.__conn.cursor()) as _cursor:
+            _cursor.execute(f'SELECT * FROM {table} WHERE {where}', args)
+            # Get column names from the cursor description
+            columns = [column[0] for column in _cursor.description]
+            rows = _cursor.fetchall()
 
-        rows = self.__cursor.fetchall()
         return [dict(zip(columns, row)) for row in rows]
 
-    def __insert(self, table, columns, data):
+    def __upsert(self, table: str, columns: List[str], data: dict) -> None:
         """
         Inserts data into table
-        :param table: str
-        :param columns: list[str]
-        :param data: dict
-        :return: void
         """
         data = self.__add_bot_name_to_args(data)
         columns = [self.__bot_name_str] + [c for c in columns if c in data]
         sql = f"INSERT OR REPLACE INTO {table} ({', '.join(columns)}) VALUES(:{', :'.join(columns)})"
-        self.__cursor.execute(sql, data)
+        with closing(self.__conn.cursor()) as _cursor:
+            _cursor.execute(sql, data)
         self.__conn.commit()
 
-    def __delete(self, table, args):
+    def __delete(self, table: str, args: dict) -> None:
         """
         Deletes data from table
-        :param table: str
-        :param args: dict
-        :return: void
         """
         args = self.__add_bot_name_to_args(args)
         where = " AND ".join(['{} = :{}'.format(c, c) for c in args])
         sql = f"DELETE FROM {table} WHERE {where}"
-        self.__cursor.execute(sql, args)
+        with closing(self.__conn.cursor()) as _cursor:
+            _cursor.execute(sql, args)
         self.__conn.commit()
 
     def get_processes(self, filters=None):
         """
         Retrieve processes from the database
-        :param filters: list[[column, relation, value]]
+        :param filters: list[(column, relation, value)]
         :return:
         """
         where = ['{} {} :{}'.format(f[0], f[1], f[0]) for f in (filters or [])]
@@ -86,7 +78,7 @@ class Database:
         return self.__select('processes', where, args)
 
     def set_process(self, process):
-        self.__insert(
+        self.__upsert(
             'processes',
             [
                 "pid",
@@ -115,7 +107,7 @@ class Database:
         return json.loads(data[0]['data'])
 
     def store_value(self, key, data):
-        self.__insert(
+        self.__upsert(
             'storage',
             ['storageKey', 'data'],
             {'storageKey': key, 'data': json.dumps(data)}
